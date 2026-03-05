58134edd59b99edf2b560fd373058a14fb7b1a13
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-03-04 18:45:52 -0700

    better stringification of nodes

--- src/yrig/maya_api/node.py
+++ src/yrig/maya_api/node.py
@@ -99,6 +99,9 @@ class Node:
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name='{self.name}')"
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class AimMatrixNode(Node):
     """Maya aimMatrix node with enhanced interface."""

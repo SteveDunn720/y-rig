dd45580cfdeb95fd4506db7822266d4487dc8f34
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-03-19 14:54:23 -0600

    handle symlinks when getting NXT directory

--- src/yrig/build/nxt_api.py
+++ src/yrig/build/nxt_api.py
@@ -6,7 +6,9 @@ import nxt
 
 log = logging.getLogger(__name__)
 
-YRIG_NXT_DIR = Path(__file__).parents[3] / "nxt"
+YRIG_NXT_DIR = (  # Get the path (resolve symlinks first though)
+    Path(__file__).resolve().parents[3] / "nxt"
+)
 os.environ["YRIG_NXT_DIR"] = str(YRIG_NXT_DIR.resolve())
 
 

b93967e7d866e790a425035875fbca22f17f6952
SteveDunn720 <sldunn0501@gmail.com>
2026-05-15 09:55:53 -0600

    renamed_ eye module

--- src/yrig/component/y_eye/eye.py
+++ src/yrig/component/y_eye/eye.py
@@ -1,9 +1,9 @@
 from yrig.control import create_control
 from yrig.joint import create_joint
 from yrig.transform import create_transform
-from yrig.component.y_eye_01.eyelid import Eyelid
-from yrig.component.y_eye_01.socket import Socket
-from yrig.component.y_eye_01.eyeball import Eyeball
+from yrig.component.y_eye.eyelid import Eyelid
+from yrig.component.y_eye.socket import Socket
+from yrig.component.y_eye.eyeball import Eyeball
 import maya.cmds as cmds
 from yrig.maya_api.node import (
     BlendColorsNode,

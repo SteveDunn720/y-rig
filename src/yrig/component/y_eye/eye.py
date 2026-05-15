104b617059307abac4da165d906b547723e20043
SteveDunn720 <sldunn0501@gmail.com>
2026-05-15 12:07:21 -0600

    small adjustemnt on the eye // adding base functionality to the eye nodein nxt // adding a space switch utility function // adding aa center aim control // connect eye look controls

--- src/yrig/component/y_eye/eye.py
+++ src/yrig/component/y_eye/eye.py
@@ -1,3 +1,5 @@
+from numpy.f2py.rules import k
+from yrig.transform.matrix import matrix_constraint
 from yrig.control import create_control
 from yrig.joint import create_joint
 from yrig.transform import create_transform
@@ -5,10 +7,7 @@ from yrig.component.y_eye.eyelid import Eyelid
 from yrig.component.y_eye.socket import Socket
 from yrig.component.y_eye.eyeball import Eyeball
 import maya.cmds as cmds
-from yrig.maya_api.node import (
-    BlendColorsNode,
-    SumNode,
-)
+from yrig.maya_api.node import BlendColorsNode, SumNode, MultiplyDivideNode
 
 
 class Eye:
@@ -19,40 +18,26 @@ class Eye:
         parent: str = "face_grp",
         control_parent: str = "neck_M0_head_ctl",
         control_size: float = 1.0,
+        parent_jnt: str = "face_jnt",
+        look_parent: str = "Look_M_ctrl",
     ):
         self.part: str = part
         self.side: str = side
         self.parent: str = parent
         self.control_parent: str = control_parent
         self.control_size: float = control_size
+        self.parent_jnt: str = parent_jnt
+        self.look_parent: str = look_parent
         self.guides: dict[str, str] = {
             "root_name": f"eye_root_{side}",
             "center_piv": f"eye_center_{self.side}",
             "aim": f"eye_aim_{self.side}",
-            "eyelid_inner_corner": f"eyelid_innercorner_{self.side}",
-            "eyelid_inner_upper": f"eyelid_innerupper_{self.side}",
-            "eyelid_inner_lower": f"eyelid_innerlower_{self.side}",
-            "eyelid_corner_inner_lower": f"eyelid_corner_innerlower_{self.side}",
-            "eyelid_corner_outer_lower": f"eyelid_corner_outerlower_{self.side}",
-            "eyelid_corner_inner_upper": f"eyelid_corner_innerupper_{self.side}",
-            "eyelid_corner_outer_upper": f"eyelid_corner_outerupper_{self.side}",
-            "eyelid_mid_upper": f"eyelid_upper_{self.side}",
-            "eyelid_mid_lower": f"eyelid_lower_{self.side}",
-            "eyelid_outer_upper": f"eyelid_outerupper_{self.side}",
-            "eyelid_outer_lower": f"eyelid_outerlower_{self.side}",
-            "eyelid_outer_corner": f"eyelid_outercorner_{self.side}",
             "eyelid_upper_curve": f"eyelid_upper_curve_{self.side}",
             "eyelid_lower_curve": f"eyelid_lower_curve_{self.side}",
-            "socket_inner_corner": f"socket_innercorner_{self.side}",
-            "socket_inner_upper": f"socket_innerupper_{self.side}",
-            "socket_inner_lower": f"socket_innerlower_{self.side}",
-            "socket_mid_upper": f"socket_upper_{self.side}",
-            "socket_mid_lower": f"socket_lower_{self.side}",
-            "socket_outer_upper": f"socket_outerupper_{self.side}",
-            "socket_outer_lower": f"socket_outerlower_{self.side}",
-            "socket_outer_corner": f"socket_outercorner_{self.side}",
             "socket_upper_curve": f"socket_upper_curve_{self.side}",
             "socket_lower_curve": f"socket_lower_curve_{self.side}",
+            "socket_mid_upper": f"socket_upper_{self.side}",
+            "socket_mid_lower": f"socket_lower_{self.side}",
             "eye_diam": f"eye_circ_{self.side}",
             "iris_diam": f"iris_circ_{self.side}",
             "pupil_diam": f"pupil_circ_{self.side}",
@@ -83,7 +68,7 @@ class Eye:
     def create_joints(self) -> None:
         self.main_jnt = create_joint(
             name=self.guides["root_name"],
-            parent=self.main_grp,
+            parent=self.parent_jnt,
             transform=self.main_ctrl.transform,
         )
 
@@ -118,15 +103,6 @@ class Eye:
 
         self.socket.build_socket()
 
-        """for side in ["outer", "inner"]:
-            matrix_constraint(
-                source_transform=self.eyelid.main_eyelid_controls[
-                    f"{side}_corner_eyelid_ctrl"
-                ].transform,
-                constrain_transform=self.socket.major_controls[f"socket_{side}_corner_ctrl"].offset,
-                keep_offset=True,
-            )"""
-
         self.eyeball = Eyeball(
             side=self.side,
             guides=self.guides,
@@ -141,10 +117,18 @@ class Eye:
         self.eyeball.build_eyeball()
 
         self.eye_look_offset_node = BlendColorsNode(name=f"look_blend_{self.side}_BC")
-        # self.eye_look_offset_node.color1.connect_from(f"{self.eyeball.look_root}.rotate")
+
+        if self.side == "R":
+            mirroraim = MultiplyDivideNode(name=f"{self.side}_aim_mirror_MD")
+            mirroraim.input2.set((-1, -1, -1))
+            mirroraim.input1.connect_from(f"{self.eyeball.look_root}.rotate")
+            # add_node.input[0].connect_from(f"{self.eyeball.look_root}.rotate{axes}")
         for axes in ["X", "Y", "Z"]:
             add_node = SumNode(name=f"look_{axes}_{self.side}_ADL")
-            add_node.input[0].connect_from(f"{self.eyeball.look_root}.rotate{axes}")
+            if self.side == "R":
+                add_node.input[0].connect_from(f"{mirroraim.output}{axes}")
+            else:
+                add_node.input[0].connect_from(f"{self.eyeball.look_root}.rotate{axes}")
             add_node.input[1].connect_from(f"{self.eyeball.eye_ctrl}.rotate{axes}")
             if axes == "X":
                 color = "R"
@@ -157,3 +141,9 @@ class Eye:
         self.eye_look_offset_node.blender.set(0.1)
         self.eye_look_offset_node.color2.set((0, 0, 0))
         self.eye_look_offset_node.output.connect_to(f"{self.eyelid.look_offset}.rotate")
+
+        matrix_constraint(
+            source_transform=self.look_parent,
+            constrain_transform=self.eyeball.look_ctrl.offset,
+            keep_offset=True,
+        )

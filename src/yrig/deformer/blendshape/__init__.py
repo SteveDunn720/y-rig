ef1b0cec8fbd7bd8d409caf1415ad16cc49ae07b
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-08-25 11:16:04 -0600

    temp removal of blendshape transfer code

--- src/yrig/deformer/blendshape/__init__.py
+++ src/yrig/deformer/blendshape/__init__.py
@@ -1,10 +1,9 @@
-from . import core, transfer
-from .core import build_blendshape_networks, export_blendshape, import_blendshape
+from . import core
+from .core import export_blendshape, import_blendshape
 
 __all__ = [
     "build_blendshape_networks",
     "core",
     "export_blendshape",
     "import_blendshape",
-    "transfer",
 ]
